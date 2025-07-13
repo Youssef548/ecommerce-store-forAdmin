5050bf689f9717b3785f3cd1e1717345c11d77d0
Youssef548 <youssefahmedpvp@gmail.com>
2025-07-13 05:07:07 +0300

    feat: add product gallery components
    - Add Gallery component with tabbed image navigation
    - Add GalleryTab component with selection highlighting
    - Implement responsive design with thumbnail grid
    - Use Headless UI for accessible tab functionality
    - Support for multiple product images with smooth transitions

---
+++ components/gallery/index.tsx
@@ -0,0 +1,38 @@
+import { Image as ImageType } from "@/types";
+import { TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
+import Image from "next/image";
+import GalleryTab from "./gallery-tab";
+
+interface GalleryProps {
+  images: ImageType[];
+}
+
+const Gallery = ({ images }: GalleryProps) => {
+  return (
+    <TabGroup as="div" className="flex flex-col-reverse w-full lg:w-1/2">
+      <div className="mx-auto mt-6 w-full max-w-2xl sm:block lg:max-w-none">
+        <TabList className="grid grid-cols-4 gap-6">
+          {images.map(image => (
+            <GalleryTab key={image.id} image={image} />
+          ))}
+        </TabList>
+      </div>
+      <TabPanels className="aspect-square w-full">
+        {images.map(image => (
+          <TabPanel key={image.id}>
+            <div className="aspect-square relative h-full w-full sm:rounded-lg overflow-hidden">
+              <Image
+                fill
+                src={image.url}
+                alt="Image"
+                className="object-cover object-center"
+              />
+            </div>
+          </TabPanel>
+        ))}
+      </TabPanels>
+    </TabGroup>
+  );
+};
+
+export default Gallery;
